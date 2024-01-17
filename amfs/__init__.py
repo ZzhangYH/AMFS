abf71f7f195094f9d56072391d4ceae6d72aacac
Yuhan Zhang <121049397+ZzhangYH@users.noreply.github.com>
2024-01-17 17:16:31 +0800

    feat: Index page with base html & css

--- amfs/__init__.py
+++ amfs/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -28,5 +28,10 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return "Hello, World!"
+
+    # index page
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
     return app
