53e456e0ca6a8545c09a240c62b202ac9d927051
Yuhan Zhang <121049397+ZzhangYH@users.noreply.github.com>
2024-02-23 19:28:46 +0800

    feat: Upload test case design to db

--- amfs/setup.py
+++ amfs/setup.py
@@ -1,9 +1,12 @@
+import os
 import tempfile
 from pathlib import Path
 
 from flask import (
     Blueprint, redirect, render_template, request, session, url_for, flash
 )
+
+from amfs.database.db import get_db
 
 bp = Blueprint('setup', __name__, url_prefix='/setup')
 
@@ -53,21 +56,35 @@ def test_case_design():
                 test.save(temp_file_dir / test.filename)
 
         elif 'total_tests' in request.form:
+            db = get_db()
             total_tests = int(request.form['total_tests'])
-            for i in range(1, total_tests + 1):
-                filename_key = f"tc_file_{i}"
-                feedback_key = f"tc_feedback_{i}"
-                mark_key = f"tc_mark_{i}"
 
-                filename = request.form.get(filename_key)
-                feedback = request.form.get(feedback_key)
-                mark = request.form.get(mark_key)
+            for i in range(1, total_tests + 1):
+                filename = request.form.get(f"tc_file_{i}")
+                feedback = request.form.get(f"tc_feedback_{i}")
+                mark = request.form.get(f"tc_mark_{i}")
 
                 with open(temp_file_dir / filename) as f:
-                    print(f"tc_id: {i}, filename: {filename}, mark: {mark}\n",
-                          f"file: {f.read()}\n",
-                          f"feedback: {feedback}\n")
+                    # Insert test case entries
+                    db.execute("""
+                        INSERT INTO TestCase (tc_id, tc_name, tc_mark, tc_input)
+                        VALUES (?, ?, ?, ?)
+                    """, (i, os.path.splitext(os.path.basename(filename))[0], mark, f.read()), )
+                    # Insert feedback entries
+                    db.execute("INSERT INTO Feedback (fb_id, fb_content) VALUES (?, ?)",
+                               (i, feedback),)
+                    # Update 1-1 feedback selection
+                    db.execute("INSERT INTO FeedbackSelection (fb_id, tc_id) VALUES (?, ?)",
+                               (i, i),)
+                    # Batch commit all changes
+                    db.commit()
 
-            return "Printed to console!"
+                # Remove temporarily saved test files
+                try:
+                    os.remove(temp_file_dir / filename)
+                except FileNotFoundError:
+                    pass
+
+            return "Check database!"
 
     return render_template('setup/test-case-design.html', tests=tests)
